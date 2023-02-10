06a65e9e87a156cce5f708e1979ed9f7f3ca5229
XueFeng <77104643+Lns-XueFeng@users.noreply.github.com>
2023-02-10 17:49:47 +0800

    Update kuwo_spider.py

--- kuwo_spider/kuwo_spider.py
+++ kuwo_spider/kuwo_spider.py
@@ -13,18 +13,12 @@ import sqlite3
 
 
 def inert_data(data):
-    # 连接数据库(如果不存在则创建)
     conn = sqlite3.connect('Newkuwo_SongUrls.db')
-    # 创建游标
     cursor = conn.cursor()
-    # 插入数据 e2
     sql = "INSERT INTO songUrls(name, songName, songUrl) VALUES(?, ?, ?)"
     cursor.execute(sql, data)
-    # 提交事物
     conn.commit()
-    # 关闭游标
     cursor.close()
-    # 关闭连接
     conn.close()
 
 
@@ -32,11 +26,11 @@ class KuWoSpider(feapder.AirSpider):
     pages = 1
 
     def start_requests(self):
-        for i in range(1,177): # 歌手页
+        for i in range(1,177):   # 歌手页
             params = {
-                "category": "0", # 0是全部，1是华语男，2是华语女，以此类推
+                "category": "0",   # 0是全部，1是华语男，2是华语女，以此类推
                 "prefix": "",
-                "pn": i, # 翻页
+                "pn": i,   # 翻页
                 "rn": "102",
                 "httpsStatus": "1",
                 "reqId": "888fc650-96ba-11ec-a249-1b3d13c1e635"
@@ -78,9 +72,9 @@ class KuWoSpider(feapder.AirSpider):
         # print(response.json)
         results = response.json['data']['artistList']
         for result in results:
-            id = str(result['id'])  # 每一位歌手的唯一id
+            id = str(result['id'])   # 每一位歌手的唯一id
             name = str(result['name'])
-            print(name) #在这里输出歌手名字包含下面循环他的歌曲页，包含
+            print(name)   # 在这里输出歌手名字包含下面循环他的歌曲页，包含
 
             musicnum = int(result['musicNum'])
             pages = int(musicnum/30) + 1
@@ -90,7 +84,7 @@ class KuWoSpider(feapder.AirSpider):
                 url = ' http://www.kuwo.cn/api/www/artist/artistMusic?'
                 params = {
                     "artistid": id,
-                    "pn": i, #翻页
+                    "pn": i,   # 翻页
                     "rn": "30",
                     "httpsStatus": "1",
                     "reqId": "b23fbe00-9909-11ec-a249-1b3d13c1e635"
@@ -106,9 +100,8 @@ class KuWoSpider(feapder.AirSpider):
             songname = str(result['name'])
             songurl = 'https://link.hhtjim.com/kw/{}.mp3'.format(rid)
             print(songname +':'+ songurl)
-            # 入库
             data = (name, songname, songurl)
-            inert_data(data)
+            inert_data(data)   # 入库
 
 
 if __name__ == "__main__":
